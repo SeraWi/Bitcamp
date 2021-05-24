6aa643b1ccba9ddf778be3e08c028f27e80bdeca
SeraWi <wsr2357@naver.com>
2021-05-24 17:55:59 +0900

    Exercise 02

--- java_project/MyProject/src/myJava0524/Member.java
+++ java_project/MyProject/src/myJava0524/Member.java
@@ -1,8 +1,14 @@
 package myJava0524;
 
 public class Member {
-	
 	public static void main(String[] args) {
 		System.out.println("Sera Wi");
+		String name = "Sera";
+		int age= 20;
+		double height = 165.3; 
+		boolean hasBook = true;
+		
+		System.out.println(name+"의 나이는 "+age+"이고 키는"+ height+"이다 ");
+		System.out.println(name+"는 자바책을 가지고 있다 : "+ hasBook);
 	}
 }
---
+++ java_project/MyProject/src/myJava0524/Person.java
@@ -0,0 +1,27 @@
+package myJava0524;
+
+import java.util.Scanner;
+
+public class Person {
+	String name; // 회원이름 저장
+	String phoneNum; // 회원 전화번호 저장
+	String id; // 회원 주민번호 저장
+	Person(String name, String phoneNum, String id){
+		this.name = name;
+		this.phoneNum = phoneNum;
+		this.id = id;
+	}
+	public static void main(String[] args) {
+		Person p1 = new Person("Sera","010-1234-5678","987654-3210000");
+	
+		System.out.println("회원 이름:"+p1.name);
+		System.out.println("회원 전화번호: "+p1.phoneNum);
+		System.out.println("회원 주민번호: "+p1.id);
+		
+		Person p2 = new Person("Julie","010-1234-5678","9876543210000");
+		System.out.println("회원이름: "+ p2.name);
+		System.out.println("회원 전화번호: "+p2.phoneNum);
+		System.out.println("회원 주민번호: "+p2.id);
+	}
+}
+
