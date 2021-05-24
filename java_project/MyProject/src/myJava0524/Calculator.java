76b4f515e702b981c60077549f2bf358a721fbe1
SeraWi <wsr2357@naver.com>
2021-05-24 16:26:55 +0900

    edit

---
+++ java_project/MyProject/src/myJava0524/Calculator.java
@@ -0,0 +1,24 @@
+package myJava0524;
+
+public class Calculator {
+	// 메소드 -> 기능(재료 -> 처리 -> 반환)
+	// 반환타입 메소드 이름(매개변수들){처리 구문}
+	void add ( int a, int b) {
+		System.out.println(a + b);
+	}
+	void subtract( int a, int b) {
+		System.out.println(a - b);
+	}
+	void multiply( int a, int b) {
+		System.out.println(a * b);
+	}
+	void devide( int a, int b) {
+		System.out.println(a / b);
+	}
+
+	public static void main(String[] args) {
+		// 인스턴스 생성-> 클래스의 정의가 필요하다. 
+		// 클래스 이름 참조변수 = new 클래스 이름
+		Calculator calc = new Calculator();
+	}
+}
