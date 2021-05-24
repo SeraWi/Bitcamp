e13736a5332d3bcd3743de5f9d533f278e593f69
SeraWi <wsr2357@naver.com>
2021-05-24 18:08:15 +0900

    Exercise 02

--- java_project/MyProject/src/myJava0524/Calculator.java
+++ java_project/MyProject/src/myJava0524/Calculator.java
@@ -1,5 +1,7 @@
 package myJava0524;
 
+import java.util.Scanner;
+
 public class Calculator {
 	// 메소드 -> 기능(재료 -> 처리 -> 반환)
 	// 반환타입 메소드 이름(매개변수들){처리 구문}
@@ -15,14 +17,30 @@ public class Calculator {
 	void devide( int a, int b) {
 		System.out.println(a / b);
 	}
+	double circle( int r) {
+		return 2 * r * Math.PI;
+	}
+	double circleArea( int r) {
+		return  Math.PI * r * r;
+	}
 
 	public static void main(String[] args) {
 		// 인스턴스 생성-> 클래스의 정의가 필요하다. 
 		// 클래스 이름 참조변수 = new 클래스 이름
+		Scanner scanner = new Scanner(System.in);
+		
 		Calculator calc = new Calculator();
 		calc.add(1, 2); //3
 		calc.subtract(5, 3); //2
 		calc.multiply(4, 5); //20
 		calc.devide(10, 3); //3
+		
+		System.out.print("반지름을 입력세요 > ");
+		int r = scanner.nextInt();
+		System.out.println("원의 둘레는 : "+calc.circle(r));
+		System.out.println("원의 넓이는 : "+calc.circleArea(r));
+		
+		
+		
 	}
 }
