29d4ab2e45cf9d33e4eac3593e73bc76c4d46de1
SeraWi <wsr2357@naver.com>
2021-05-25 14:55:27 +0900

    .

--- java_project/MyProject/src/chapter03/Calculator2.java
+++ java_project/MyProject/src/chapter03/Calculator2.java
@@ -1,5 +1,7 @@
 package chapter03;
 
+import java.util.Scanner;
+
 public class Calculator2 {
 
 	// 클래스 이름 -> 대문자 시작, 케멀케이스
@@ -33,7 +35,22 @@ public class Calculator2 {
 	public static void main(String[] args) {
 		
 		Calculator2  cal = new Calculator2();
-		System.out.println(cal.PI);
-		System.out.println("1 + 2 ="+cal.plus(1,2));
+		//System.out.println(cal.PI);
+		//System.out.println("1 + 2 ="+cal.plus(1,2));
+		
+		// Scanner : 자원을 프로그램으로 받아오는 역할
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("덧셈을 합니다. \n숫자1을 입력해주세요");
+		int num1 = scanner.nextInt();
+		System.out.println("숫자2를 입력해주세요.");
+		int num2 = scanner.nextInt();
+		System.out.println(num1 +" + "+ num2 + " = " + cal.plus(num1, num2));
+		
+		System.out.println("반지름을 입력해주세요.");
+		float r = scanner.nextFloat();
+		System.out.println("입력 받은 반지름의 길이:" + r);
+		System.out.println("원의 둘레:"+ cal.circum(r));
+		System.out.println("원의 넓이:"+ cal.cirArea(r));
 	}
 }
