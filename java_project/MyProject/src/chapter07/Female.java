b145be6e8d23fcdc1a26a815d8cb2898cd9ccbf1
SeraWi <wsr2357@naver.com>
2021-05-26 17:05:21 +0900

    .

---
+++ java_project/MyProject/src/chapter07/Female.java
@@ -0,0 +1,22 @@
+package chapter07;
+
+public class Female extends Person{
+	String gender;
+	String major;
+	String phoneNum;
+
+	Female(String name, int birthYear, String gender,String major,String phoneNum){
+		super(name, birthYear);
+		this.gender = gender;
+		this.major = major;
+		this.phoneNum = phoneNum;
+	}
+	void personInfo() {
+		super.personInfo();
+		System.out.println("저는 "+gender+"입니다.");
+		System.out.println("전화번호는  "+ phoneNum+"입니다.");
+	}
+	void studying() {
+		System.out.println("전공은 "+major+"입니다.");
+	}
+}
