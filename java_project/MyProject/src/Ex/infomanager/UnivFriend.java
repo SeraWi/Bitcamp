aec89fb22f4fd96d5d6d6171b33d665b635f6f76
SeraWi <wsr2357@naver.com>
2021-05-28 17:02:20 +0900

    Ex_FriendBook

---
+++ java_project/MyProject/src/Ex/infomanager/UnivFriend.java
@@ -0,0 +1,25 @@
+package Ex.infomanager;
+
+public class UnivFriend extends Friend{
+	
+	// 대학 친구는 전공정보를 가진다.
+	String major;
+	
+	public UnivFriend(String name, String phoneNumber, String address,String major) {
+		super(name, phoneNumber, address);
+		this.major = major;
+	}
+
+	@Override
+	public void showData() {
+		super.showData();
+		System.out.println("전     공: " + major);
+	}
+
+	@Override
+	public void showBasicInfo() {
+		System.out.println("이      름: "+ name);
+		System.out.println("전      화: " + phoneNumber);
+		System.out.println("전      공: "+ major);
+	}
+}
