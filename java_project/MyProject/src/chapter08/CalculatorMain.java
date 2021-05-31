3031b0d921f5bdc2439e29e5bcf163f97a286973
SeraWi <wsr2357@naver.com>
2021-05-31 13:12:08 +0900

    edit classname

--- java_project/MyProject/src/chapter08/CalculatorMain.java
+++ java_project/MyProject/src/chapter08/CalculatorMain.java
@@ -4,7 +4,7 @@ public class CalculatorMain {
 
 	public static void main(String[] args) {
 		
-		Calculator calc =  new Calculate();
+		Calculator calc =  new Calculator_Impl();
 		
 		System.out.println(calc.add(2480,896));
 		System.out.println(calc.substract(2480,896));
