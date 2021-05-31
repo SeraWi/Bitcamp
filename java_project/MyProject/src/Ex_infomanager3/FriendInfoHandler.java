8912a5534741a07c14509728e837043b7cdcd3d8
SeraWi <wsr2357@naver.com>
2021-05-31 16:55:04 +0900

    array->arrayList

--- java_project/MyProject/src/Ex_infomanager3/FriendInfoHandler.java
+++ java_project/MyProject/src/Ex_infomanager3/FriendInfoHandler.java
@@ -1,18 +1,20 @@
-package Ex_infomanager2;
+package Ex_infomanager3;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FriendInfoHandler {
 
 	// 저장 데이터(객체)를 배열로 저장하고 관리하는 기능을 가지는 클래스
-	
-	Friend[] friends ;
-	int numOfFriend; //배열의 입력 index 역할, 정보의 개수
+	// 2021-05-21
+	// 배열 -> 컬렉션
+	ArrayList<Friend> friends;
 
 	//생성자
 	public FriendInfoHandler(int num) { //배열의 사이즈 -> 배열 인스턴스 생성
-		this.friends = new Friend[num];
-		this.numOfFriend = 0;
+		//this.friends = new Friend[num];
+		//this.numOfFriend = 0;
+		friends = new ArrayList<Friend>(num);
 	}
 	// 데이터의 입력 처리
 	// 사용자로부터 친구의 정보를 입력 받아서 -> 인스턴스를 생성 -> 배열에 저장한다. 
@@ -51,7 +53,9 @@ public class FriendInfoHandler {
 	}
 	//Friend 클래스를 상속하는 타입의 인스턴스를 받아 배열에 저장하기
 	public void addFriendInfo(Friend f) {
-		friends[numOfFriend++] = f;
+		//friends[numOfFriend++] = f;
+		//컬렉션: List에 요소 추가
+		friends.add(f);
 		System.out.println("정보가 저장되었습니다.");
 	}
 	// 모든 데이터를 출력하는 메소드
@@ -59,9 +63,12 @@ public class FriendInfoHandler {
 	public void showAllData() {
 		System.out.println("친구의 모든 정보를 출력합니다.");
 		System.out.println("=======================");
-		if(numOfFriend > 0) {
-			for(int i =0 ; i < numOfFriend; i++) {
-				friends[i].showData();
+		
+		// 2021-05-31: 수정
+		// 요소의 저장방식이 배열->collection->반복문 수정
+		if(friends.size() > 0) {
+			for(int i =0 ; i < friends.size(); i++) {
+				friends.get(i).showData();
 				System.out.println("=======================");
 			}
 		}else {
@@ -72,9 +79,12 @@ public class FriendInfoHandler {
 	public void showAllBasicData() {
 		System.out.println("친구의 모든 기본 정보를 출력합니다.");
 		System.out.println("=======================");
-		if(numOfFriend > 0) {
-			for(int i =0 ; i < numOfFriend; i++) {
-				friends[i].showBasicInfo();
+		
+		// 2021-05-31: 수정
+		// 요소의 저장방식이 배열->collection->반복문 수정
+		if(!friends.isEmpty()) {// isEmpty(): 요소의 사이즈 0일때 true 반환 
+			for(int i =0 ; i < friends.size(); i++) {
+				friends.get(i).showBasicInfo();
 				System.out.println("=======================");
 			}
 		}else {
