c579eb2bebeee8735381c289268470724efa0a63
SeraWi <wsr2357@naver.com>
2021-05-31 14:57:37 +0900

    Update CalculatorMain.java

--- java_project/MyProject/src/chapter08/CalculatorMain.java
+++ java_project/MyProject/src/chapter08/CalculatorMain.java
@@ -10,7 +10,5 @@ public class CalculatorMain {
 		System.out.println(calc.substract(2480,896));
 		System.out.println(calc.multiply(2480325,896));
 		System.out.println(calc.divide(2480,896));
-		
 	}
-
 }
