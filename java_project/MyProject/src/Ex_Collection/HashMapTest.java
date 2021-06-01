51df6d037cf24736d5c4fadecb158745779002ba
SeraWi <wsr2357@naver.com>
2021-06-01 12:02:59 +0900

    Create HashMapTest.java

---
+++ java_project/MyProject/src/Ex_Collection/HashMapTest.java
@@ -0,0 +1,27 @@
+package Ex_Collection;
+
+import java.util.*;
+
+public class HashMapTest {
+
+	public static void main(String[] args) {
+		// Map: key, value 형식으로 저장
+		// key -> 저장한 데이터를 식별하기 위한 값, 중복 허용X
+		// value -> 저장하고자 하는 데이터
+		
+		HashMap<Integer, String> map = new HashMap<>();
+		
+		//데이터 저장:put(e)
+		
+		map.put(new Integer(1), "일번");
+		map.put(5, "오번"); //auto boxing
+		map.put(8, "팔번");
+		
+		//데이터 탐색
+		System.out.println(map.get(new Integer(5)));
+		System.out.println(map.get(1));
+		System.out.println(map.get(8));
+		System.out.println(map.get(10));//없는 데이터 -> null 나온다
+		
+	}
+}
