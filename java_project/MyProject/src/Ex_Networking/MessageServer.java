51d55edec0280bb72298550cd05acd44be8da012
SeraWi <wsr2357@naver.com>
2021-06-04 16:13:41 +0900

    Server and Client

--- java_project/MyProject/src/Ex_Networking/MessageServer.java
+++ java_project/MyProject/src/Ex_Networking/MessageServer.java
@@ -8,28 +8,35 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class MessageServer {
+	// 서버에서 먼저 메시지 보내고 -> client가 답변
 
 	public static void main(String[] args) {
-		Socket s = null;
+		Socket s = null; 
 		ServerSocket ss = null;
 		DataInputStream din = null;
 		DataOutputStream dout = null;
 
 		try {
 			ss = new ServerSocket(8888);
+			System.out.println("서버실행");
+			System.out.println("--------------------------");
 			s = ss.accept();
-
+			System.out.println("Client 접속");
+			System.out.println("--------------------------");
+			
+			
 			din = new DataInputStream(s.getInputStream());
 			dout = new DataOutputStream(s.getOutputStream());
 
 			Scanner sc = new Scanner(System.in);
 			String str1 = null; // 받는 메시지
-			String str2 = null; // 보내는 메시지
+			String str2 = ""; // 보내는 메시지
 
 
 			while(str2 .equals("exit")) {
 				str1 = din.readUTF();
-				System.out.println(str1);
+				System.out.println("client Message: "+ str1);
+				
 				str2 = sc.nextLine();
 				dout.writeUTF(str2);
 				dout.flush();
@@ -40,38 +47,12 @@ public class MessageServer {
 
 			e.printStackTrace();
 		}finally {
-			if(din != null) {
-				try {
-					din.close();
 
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				if(dout !=  null) {
-					try {
-						dout.close();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-				if( s != null) {
-					try {
-						s.close();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-				if( ss != null) {
-					try {
-						ss.close();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}	
-				}
-			}
+			Util.close(dout);
+			Util.close(din);
+			Util.close(s);
+			Util.close(ss);
 		}
 	}
 }
+
