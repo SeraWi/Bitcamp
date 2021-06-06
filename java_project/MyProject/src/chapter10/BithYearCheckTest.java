a3a899abaffa335d1e37875093f4e738482b4ef4
SeraWi <wsr2357@naver.com>
2021-06-06 14:16:35 +0900

    chapter10

---
+++ java_project/MyProject/src/chapter10/BithYearCheckTest.java
@@ -0,0 +1,24 @@
+package chapter10;
+
+import java.util.Scanner;
+
+public class BithYearCheckTest {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("태어난 해를 입력해주세요.");
+
+		try {
+			int birthYear = scanner.nextInt();
+			if(birthYear <= 0 || birthYear > 2021) {
+				throw new BirthYearException("잘못 입력되었습니다.");
+			}
+		}catch(BirthYearException ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+}
+class BirthYearException extends Exception{
+	BirthYearException(String msg){
+		super(msg);
+	}
+}
