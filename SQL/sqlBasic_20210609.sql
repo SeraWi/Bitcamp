d0b601bde0643043af22a08bd1326594dafdbdc9
SeraWi <wsr2357@naver.com>
2021-06-09 17:34:51 +0900

    Update sqlBasic_20210609.sql

--- SQL/sqlBasic_20210609.sql
+++ SQL/sqlBasic_20210609.sql
@@ -6,4 +6,32 @@
 select * from tab;
 
 -- 테이블의 정보를 검색 : 컬럼의 이름, null유무, 타입, 사이즈
-Desc dept;
+desc dept;
+desc emp;
+
+-- 데이터 조회를 위한 명령 : select 
+-- select {컬럼명,...,*} from table name;
+
+select * from emp; --emp 테이블의 모든 데이터
+
+-- 필요한 컬럼을 출력
+-- 사원의 사원 번호와 사원 이름을 출력
+select ename, empno from emp;
+
+-- select 컬럼의 산술 연산이 가능 : 컬럼과 숫자의 연산, 컬럼과 컬럼간의 연산가능
+-- 사원 이름, 월 급여, 연봉계산 결과값(sal * 12)
+select ename,sal, sal*12 as ysal from emp;
+
+select sal, comm, sal*comm, sal *12, sal/10, sal -100, sal+1000 from emp;
+
+-- 사원의 이름, 직급, 급여, 커미션, 연봉(급여 * 12), 연봉(급여 *12+커미션)
+select ename, job, sal, comm, sal*12 as ysal1, sal*12+comm as ysal2 from emp;
+
+-- 개선: null값을 0으로 지환해서 연산
+-- nvl(컬럼 이름, 기본값) : 컬럼의 값이 null일때 기본값으로 치환 
+select ename, job, sal, nvl(comm,0), sal*12 as ysal1, sal*12+ nvl(comm,0) as ysal2 from emp;
+
+-- 컬럼과 문자열을 붙이는 연산 || 이용
+-- sql 에서 문자열 표현 ->' 작은 따옴표 이용'
+select ename || 'is a '|| job from emp;
+
