d6815f0b4e8672e8c06a6962ab3fff172522b376
SeraWi <wsr2357@naver.com>
2021-06-11 11:02:04 +0900

    Update 0611_function2.sql

--- SQL/sql_Class/0611_function2.sql
+++ SQL/sql_Class/0611_function2.sql
@@ -15,7 +15,28 @@ select * from orders;
 select orderid, orderdate, to_char(orderdate, 'YYYY.MM.DD.')
 from orders;
 
+-- 숫자->문자
+select to_char(123456,'0000000000'), to_char(123456,'9999999999')
+from dual;
+--﻿결과 0000123456, 123456
+select to_char(123456,'0,000,000,000'), to_char(123456,'L9,999,999,999')
+from dual;
+
+select ename, sal, to_char(sal*1100,'L999,999,999')
+from emp;
+
+-- 문자 -> 숫자
+-- to_number(문자열,패턴)
+select to_number('1,000,000','9,999,999'),
+        to_number('1,000,000','9,999,999')+100000
+from dual;
+
+-- 문자 ->날짜
+-- to_date(문자열,패턴)
+select to_date('2012.05.17','YYYY.MM.DD'),trunc(sysdate- to_date('2012.05.17','YYYY.MM.DD'))/365
+from dual;
 
 
 
--- 문자 -> 숫자, 문자 -> 날짜
+
+
