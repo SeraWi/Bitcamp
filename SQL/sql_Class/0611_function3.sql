ef07d2ff5cc443e2b2af432066292b733f0e8725
SeraWi <wsr2357@naver.com>
2021-06-11 12:01:11 +0900

    Update 0611_function3.sql

--- SQL/sql_Class/0611_function3.sql
+++ SQL/sql_Class/0611_function3.sql
@@ -1,3 +1,26 @@
 --2021.06.11
 
---그룹함수
+-- 그룹함수
+-- 여러행을 묶어 처리하는 함수
+-- sum,avg,count,max, min
+
+-- 사원 테이블에서
+-- 1. 사원의 수
+-- 2. 사원의 급여 총합
+-- 3. 급여 평균
+-- 4. 최대 급여
+-- 5. 최소 급여
+
+select count(*)
+from emp;
+-- 행의 개수
+select count(comm)
+from emp;
+-- 행에 null이 있다면 count하지 않는다.
+
+select count(*) as "사원의 수",
+        sum(sal) as "급여 총합", 
+        avg(sal) as "급여 평균",
+        max(sal) as "최대 급여",
+        min(sal) as "최소 급여"
+from emp;
