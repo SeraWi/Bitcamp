6fd4b65ee1b6b9673256d4007457b15b4c43cd17
SeraWi <wsr2357@naver.com>
2021-06-11 11:19:55 +0900

    Update 0611_function2.sql

--- SQL/sql_Class/0611_function2.sql
+++ SQL/sql_Class/0611_function2.sql
@@ -36,7 +36,40 @@ from dual;
 select to_date('2012.05.17','YYYY.MM.DD'),trunc(sysdate- to_date('2012.05.17','YYYY.MM.DD'))/365
 from dual;
 
+--decode함수 : 분기를 위해 사용한다.switch -case 유사
+--decode (컬럼, = 조건1 값, 조건1의 참일 때 사용할 값,
+--               조건2 값, 조건2의 참일 때 사용할 값,
+--               ...)
 
+-- emp 테이블에서 부서번호에 맞는 부서이름을 출력한다.
 
+select*
+from dept;
+
+ -- 10 ACCOUNTING
+ -- 20 RESEARCH
+ -- 30 SALES
+ -- 40 OPERATIONS
+ 
+ select ename, deptno,
+        decode(deptno, 10 ,'ACCOUNTING',
+                       20, 'RESEARCH',
+                       30, 'SALES',
+                       40,'OPERATION'
+        ) as DNAME
+ from emp
+ order by dname;
+
+-- 직급에 따라 급여를 인상하도록 하자
+-- 직급이 'ANALYST'인 사원은 5%인상, 'SALESMAN'인 사원은 10%, 'MANAGER'인 사원은 15%
+-- 'CLERK'인 사원은 20% 인상
+
+select ename,sal,
+        decode( job, 'ANALYST', sal*1.05,
+                    'SALESMAN',sal*1.1,
+                    'MANAGER', sal *1.15,
+                    'CLERK', sal * 1.2
+        ) as upsal
+from emp;
 
 
