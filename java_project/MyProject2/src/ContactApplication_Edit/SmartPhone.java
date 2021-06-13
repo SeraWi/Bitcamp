0822ee117f6cbc0e57ef421fb3c5bc8f9141d50b
SeraWi <wsr2357@naver.com>
2021-06-13 13:41:09 +0900

    Update SmartPhone.java

--- java_project/MyProject2/src/ContactApplication_Edit/SmartPhone.java
+++ java_project/MyProject2/src/ContactApplication_Edit/SmartPhone.java
@@ -15,6 +15,9 @@ import java.util.Scanner;
 //7. 파일 로드
 //8. 종료
 
+// 예외처리- 메뉴 입력시 발생, 연락처 이름 입력시 공백에 대한 예외처리, 영문자와 한글만 허용 예외처리
+// 전화번호 형식에 맞지 않을 때 예외처리, 중복될 때 예외 상황 발생
+
 public class SmartPhone {
 	ArrayList<Contact> contacts ; // ArrayList에 contact저장하기
 	Scanner scanner;
@@ -26,73 +29,113 @@ public class SmartPhone {
 		this.numOfContact = 0;
 	}
 
-	public void saveContact() {////1. 연락처 입력
+	public static void nameTest(String name) throws NameNotFindException{
+		// 연락처에 이름 입력시 공백에 대한 예외처리 -> 다시 입력받기
+		// 영문자와 한글만 허용하는 예외처리
+		// 한글 허용 해결해야함
+		try {
+			for(int i = 0; i< name.length(); i++) {
+				char nameChar = name.charAt(i);
+				if(!('a' <= nameChar && nameChar <='z' || 
+						'A' <= nameChar && nameChar <='Z' 
+						)) {
+					throw new NameNotFindException("이름에 영문자와 한글 이외에 글자가 입력되었습니다.");
+				}
+			}
+			if(name.equals("") || name == null) {
+				throw new NameNotFindException("이름이 입력되지 않았습니다.");
+			}
+
+		}catch(NameNotFindException ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	public static void phoneNumTest(String phoneNum) {
+		// 전화번호 형식에 맞지 않을 때 예외처리
+		// 전화번호가 동일할 경우 예외처리-> 입력되지 않도록 처리
+
+	}
+	
+
+	public void saveContact() throws NameNotFindException {////1. 연락처 입력
+
 		System.out.println("-----------연락처를 저장합니다.-----------");
-		System.out.print("이름을 입력해주세요 > ");
+		System.out.print("이름을 입력해주세요 >");
 		String name = scanner.nextLine();
+		// 연락처에 이름 입력시 공백에 대한 예외처리 -> 다시 입력받기
+		// 영문자와 한글만 허용하는 예외처리
+		nameTest(name);//예외처리 하는 메서드 호출-> NameNotFindException
+
 		System.out.print("전화번호를 입력해주세요>");
 		String phoneNum = scanner.nextLine();
+		// 전화번호 형식에 맞지 않을 때 예외처리
+		// 전화번호가 동일할 경우 예외처리-> 입력되지 않도록 처리
+
+
 		System.out.print("이메일을 입력해주세요>");
 		String email = scanner.nextLine();
 		System.out.print("주소를 입력해주세요>");
 		String address = scanner.nextLine();
 		System.out.print("생일을 입력해주세요>");
 		String birthday = scanner.nextLine();
-		System.out.print("그룹을 입력해주세요. (회사 혹은 고객) >");
+		System.out.print("그룹을 입력해주세요. (회사,고객, 기타) >");
 		String group = scanner.nextLine();
 		//그룹에 해당하는 정보-> companyContact or customerConatct
-		
+
 		if(group == "회사") {
-			System.out.print("회사 이름을  입력해주세요 > ");
+			System.out.print("회사 이름을  입력해주세요 >");
 			String comName = scanner.nextLine();
 			System.out.println("부서 이름을 입력해주세요 >");
 			String deptName = scanner.nextLine();
-			System.out.println("직급을 입력해주세요 > ");
+			System.out.println("직급을 입력해주세요 >");
 			String position = scanner.nextLine();
-		
+
 			contacts.add(new CompanyContact(name,phoneNum,email,address,birthday,group,comName,deptName,position));
 			System.out.println("------------저장 완료 되었습니다.-----------");
-			
-		}else {// 고객 일때
-			System.out.print("회사 이름을  입력해주세요 > ");
+
+		}else if(group == "고객"){// 고객 일때
+			System.out.print("회사 이름을  입력해주세요 >");
 			String comName = scanner.nextLine();
-			System.out.println("거래품목을 입력해주세요 >");
+			System.out.print("거래품목을 입력해주세요 >");
 			String product = scanner.nextLine();
-			System.out.println("직급을 입력해주세요 > ");
+			System.out.print("직급을 입력해주세요 >");
 			String position = scanner.nextLine();
 			contacts.add(new CustomerContact(name,phoneNum,email,address,birthday,group,comName,product,position));
-			System.out.println("------------저장 완료 되었습니다.-----------");
+			System.out.print("------------저장 완료 되었습니다.-----------");
+		}else { //기타
+			contacts.add(new Contact(name, phoneNum,email,address,birthday,group));
 		}
 	}
 
 
-	public void searchContact() { //2. 연락처 검색 -> 이름을 검색하면 전화번호가 나온다.\
-		
+	public void searchContact() { 
+		// 2. 연락처 검색 -> 이름을 검색하면 전화번호가 나온다.
+		// 검색 후 결과는
+		// 위세라 : 010 3524 8600 의 형식으로 보여준다.
+
 		System.out.println("-------연락처를 검색합니다.--------------");
-		System.out.print("이름을 입력하세요. > ");
+		System.out.print("이름을 입력하세요. >");
 		String searchName = scanner.nextLine();
-		
+
 		//equals
-		
+
 		for(int i = 0; i< contacts.size(); i++) {
 			if(searchName.equals(contacts.get(i).getName())) { //검색 이름과 비교!
-				System.out.println(contacts.get(i).getName());
-				break;
-			}else {
-				System.out.println("찾으시는 이름이 없습니다.");
+				System.out.println(contacts.get(i).getName()+" : "+contacts.get(i).getPhoneNum());
+				return;
 			}
 		}
+		System.out.println("찾으시는 이름이 없습니다.");
 	}
 
-
-
 	public void showList() {//5. 연락처 전체 리스트 보기 (간략보기)
-		// [1] 위세라 010 3524 8600 의 형식
+		// [1] 위세라 010 3524 8600 의 형식으로 보여주기
 
 		if(contacts.size() >0 ) {
 			for( int i = 0; i< contacts.size(); i++ ) {
 				int listNum = i+1;
-				System.out.println("["+ listNum +"]"+contacts.get(i).getName()
+				System.out.println("["+ listNum +"]"+contacts.get(i).getName()+" "
 						+contacts.get(i).getPhoneNum());
 			}
 
@@ -105,20 +148,21 @@ public class SmartPhone {
 		System.out.println("-----------연락처를 수정합니다.-----------");
 		showList(); //리스트 보기 먼저
 
-		System.out.println("수정할 연락처의 번호를 선택하세요.");//[1] [2] [3] [4]....
+		System.out.println("수정할 연락처의 리스트 번호를 선택하세요.");
+		//[1] [2] [3] [4]...., 리스트 번호든 이름이든 상관 없이 가능!?
 		int selNum = Integer.parseInt(scanner.nextLine());
 
 		System.out.println("수정할 연락처의 상세 보기 입니다.");
-		contacts.get(selNum-1).showData();
-		
+		contacts.get(selNum-1).showData();// 인덱스는 하나 작은 수!
+
 		System.out.println("==========================");
 		System.out.println("수정할 정보의 번호를 입력하세요.");
 		System.out.println("1. 이름  2. 전화번호 3. 이메일");
-		System.out.println("4. 주소  5. 생일       6. 그룹");
+		System.out.println("4. 주소  5. 생일    6. 그룹");
 		int editNum = Integer.parseInt(scanner.nextLine());
 		System.out.println("===========================");
 
-		switch(editNum) {
+		switch(editNum) {// 간략하게 바꾸기
 		case 1:
 			System.out.print("이름을 입력해주세요.");
 			String name = scanner.nextLine();
@@ -159,12 +203,16 @@ public class SmartPhone {
 			break;
 		}		
 	}
-	
-	
-	public void deleteContact() {// 4. 연락처 삭제
-		
-	}
 
+	public void deleteContact() {// 4. 연락처 삭제
+		showList(); //리스트 먼저 보여주기
+
+		System.out.println("삭제할 연락처 리스트 번호를 입력해주세요.");
+		int selNum = Integer.parseInt(scanner.nextLine());
+
+		contacts.remove(selNum-1); //인덱스 = 리스트 번호 -1
+		System.out.println("연락처가 삭제되었습니다.");
+	}
 
 
 }
