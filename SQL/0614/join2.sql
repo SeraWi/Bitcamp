bed62865bfe41fefee54d12cf580dfd6c9cea504
SeraWi <wsr2357@naver.com>
2021-06-14 10:23:09 +0900

    file edit

--- SQL/0614/join2.sql
+++ SQL/0614/join2.sql
@@ -8,7 +8,16 @@ select *
 from emp e, salgrade s
 order by e.ename;
 
+--sal에 따라 등급 알아보기
 select e.ename, e.sal, s.grade, s.losal,s.hisal
-from emp e, salgrade s
-where e.sal >= s.losal and e.sal <= s.hisal
+from emp e, salgrade s -- 새로운 테이블이다!!
+--where e.sal >= s.losal and e.sal <= s.hisal
+where sal between s.losal and s.hisal
 order by e.ename;
+
+
+-- ansi cross join
+select * 
+from emp cross join  dept;
+
+
