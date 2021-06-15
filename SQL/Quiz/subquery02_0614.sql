59b31e5bdfc58e9bc82bb53162ce20d3108c3f43
SeraWi <wsr2357@naver.com>
2021-06-15 10:54:06 +0900

    Update subquery02_0614.sql

--- SQL/Quiz/subquery02_0614.sql
+++ SQL/Quiz/subquery02_0614.sql
@@ -18,9 +18,10 @@ where bookid in ( select bookid
 order by publisher;
 
 --풀이2 ,natural join
-select *
-from orders natural join customer natural join orders
-where name ='박지성';
+select count(publisher)
+from orders natural join customer natural join book
+where name ='박지성'
+
 
 
 --6) 박지성이구매한도서의이름, 가격, 정가와판매가격의차이
