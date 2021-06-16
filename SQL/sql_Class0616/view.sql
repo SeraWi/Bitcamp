24d6860d3e8f1221989e0024823db78122583ab6
SeraWi <wsr2357@naver.com>
2021-06-16 18:08:03 +0900

    Update view.sql

--- SQL/sql_Class0616/view.sql
+++ SQL/sql_Class0616/view.sql
@@ -17,7 +17,39 @@ select * from emp_view30
 where ename = 'JAMES';
 
 
+--view 삭제
+drop view emp_view30;
 
+---------------------------------------------------------------------------------
+--인라인 뷰를 이용한 TOP3 구하기
+
+select rownum, ename 
+from emp
+--order by hiredate
+;
+
+select rownum, ename
+from (select * from emp order by ename)
+;
+
+-- 입사일이 빠른 사람 5명만 (TOP N)을 얻어오기
+select rownum, ename, hiredate
+from (select * from emp  order by hiredate)
+where rownum < 6
+;
+
+
+create or replace view view_HIR
+as
+select * from emp  order by hiredate;
+
+select rownum, ename, hiredate
+from view_hir;
+
+select rownum, ename, hiredate 
+from view_hir
+where rownum < 6
+;
 
 
 
