a80fd30c0c0e80d5ba44795c5f393deff74967d7
SeraWi <wsr2357@naver.com>
2021-06-16 15:49:20 +0900

    Update DML_SAMPLE_0616.sql

--- SQL/Quiz/DML_SAMPLE_0616.sql
+++ SQL/Quiz/DML_SAMPLE_0616.sql
@@ -6,4 +6,32 @@
 -- UPDATE : U PDATE
 -- DELETE : D ELETE
 
+-- INSERT : CREATE
+desc phoneInfo_basic;
+insert into phoneinfo_basic
+values(1, 'KING', '010-0000-0000', 'king@gmail.com', 'KOREA',sysdate)
+;
+
+insert into phoneinfo_basic (idx, fr_name, fr_phonenumber)
+values(2,'SCOTT','010-9999-9999');
+
+-- SELECT : READ
+select * from phoneInfo_basic;
+
+--UPDATE : UPDATE
+--SCOTT 의 이메일을 업데이트, 주소도 업데이트하기
+--SCOTT의 idx -> 2
+update phoneInfo_basic
+set fr_email= 'scott@naver.com', fr_address = '서울'
+where idx = 2;
+-- idx : pk
+
+--DELETE : DELETE
+delete from phoneinfo_basic
+where idx = 1
+;
+
+
+
+
 
