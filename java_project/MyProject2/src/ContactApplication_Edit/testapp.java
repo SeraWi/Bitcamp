625dfc09fe76ee2a19a07c906683847a068c2c78
SeraWi <wsr2357@naver.com>
2021-06-16 19:37:52 +0900

    Create testapp.java

---
+++ java_project/MyProject2/src/ContactApplication_Edit/testapp.java
@@ -0,0 +1,22 @@
+package ContactApplication_Edit;
+
+import java.util.regex.Pattern;
+
+public class testapp {
+	
+	public static boolean phoneNumTypeCheck(String phoneNum) {
+		//핸드폰 번호가 형식에 맞지 않으면 false리턴한다.
+		//핸드폰 번호는 : 000-0000-0000형식 혹은 01000000000 형식
+		return Pattern.matches("^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$" , phoneNum);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean result =phoneNumTypeCheck("010-3333-3333");
+		System.out.println(result);
+		boolean result2 =phoneNumTypeCheck("01033333333");
+		System.out.println(result2);
+	}
+
+}
