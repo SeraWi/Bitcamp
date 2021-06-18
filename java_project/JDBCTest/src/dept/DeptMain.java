51e3b08633fed3b60299fec35393122e83c3f49e
SeraWi <wsr2357@naver.com>
2021-06-18 16:20:39 +0900

    Update DeptMain.java

--- java_project/JDBCTest/src/dept/DeptMain.java
+++ java_project/JDBCTest/src/dept/DeptMain.java
@@ -1,9 +1,10 @@
 package dept;
 
+import java.io.DataOutput;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.List;
+import java.util.Scanner;
 
 public class DeptMain {
 
@@ -25,7 +26,56 @@ public class DeptMain {
 			conn= DriverManager.getConnection(jdbcUrl, user, pw);
 			System.out.println("데이터베이스 연결 성공!!");
 			
+			DeptDao dao = new DeptDao();
 			
+			// 리스트 출력 READ
+			
+//			List<Dept> list = dao.getDeptList(conn);
+//			
+//			for(Dept dept : list ) {
+//				System.out.println(dept);
+//			}
+//			*/
+//
+//---------------------------------------------------------------------------------------------			
+			// CREATE: INSERT 
+			
+			Scanner sc = new Scanner(System.in);
+//			System.out.println("부서입력을 시작합니다.");
+//			System.out.println("부서번호  부서이름 부서위치 순으로 입력해주세요.");
+//			System.out.println("예) 50 dev seoul");
+//			String deptData = sc.nextLine();
+//			String[] deptDatas = deptData.split(" "); // 공백 기준으로 잘라서 배열에 넣기
+//			
+//			// Dept 객체 생성하고 Dao 메소드 호출
+//			Dept dept = new Dept(Integer.parseInt(deptDatas[0]), deptDatas[1], deptDatas[2]);
+//			
+//		    int result = dao.insertDept(conn,dept);
+//		    if(result > 0) {
+//		    	System.out.println("입력성공");
+//		    }else {
+//		    	System.out.println("입력 실패!!");
+//		    }
+//-----------------------------------------------------------------------------------------------
+			
+		    // Update : update
+		    		    
+			System.out.println("부서수정을 시작합니다.");
+			System.out.println("수정하고자 하는 부서번호  새로운 부서이름 새로운 부서위치 순으로 입력해주세요.");
+			System.out.println("예) 50 dev seoul");
+			String deptData = sc.nextLine();
+			String[] deptDatas = deptData.split(" "); // 공백 기준으로 잘라서 배열에 넣기
+			
+			// Dept 객체 생성하고 Dao 메소드 호출
+			Dept dept = new Dept(Integer.parseInt(deptDatas[0]), deptDatas[1], deptDatas[2]);
+			
+		    int result = dao.editDept(conn,dept);
+		    if(result > 0) {
+		    	System.out.println("수정성공");
+		    }else {
+		    	System.out.println("수정 실패!!");
+		    }
+		    
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -35,13 +85,8 @@ public class DeptMain {
 		}
 		
 
-		DeptDao dao = new DeptDao();
 		
-		List<Dept> list = dao.getDeptList(conn);
 		
-		for(Dept dept : list ) {
-			System.out.println(dept);
-		}
 		
 		
 	}
