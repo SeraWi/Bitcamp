cdab3ff80eaa70b6b4b2653694407005e2d71978
SeraWi <wsr2357@naver.com>
2021-06-18 18:20:39 +0900

    Create DeptMain.java

--- java_project/JDBCTest/src/dept/DeptMain.java
+++ java_project/JDBCTest/src/dept/DeptMain.java
@@ -1,107 +1,60 @@
 package dept;
 
-import java.io.DataOutput;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Scanner;
 
+/*
+ * deptMain_ver01 간략하게 만들기
+ */
 public class DeptMain {
 
 	public static void main(String[] args) {
-		
-		Connection conn = null;
-		
-				
+
+		DeptManager manager = new DeptManager(DeptDao.getInstance());
+		Scanner sc = new Scanner(System.in);
+
 		try {
 			//1. 드라이버 로드
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			System.out.println("드라이버 로드 성공!");
 
-			//2. 연결
-			String jdbcUrl = "jdbc:oracle:thin:@localhost:1521:xe";
-			String user = "hr";
-			String pw = "tiger";
+			while(true) {
+				System.out.println("부서관리 프로그램");
+				System.out.println("---------------------------------------");
+				System.out.println("1. 부서 리스트 ");
+				System.out.println("2. 부서 정보 입력");
+				System.out.println("3. 부서 정보 수정");
+				System.out.println("4. 부서 정보 삭제");
+				System.out.println("5. 프로그램 종료");
+				System.out.println("---------------------------------------");
+				System.out.println("원하시는 기능의 번호를 입력해주세요.");
+				int num = Integer.parseInt(sc.nextLine());
 
-			conn= DriverManager.getConnection(jdbcUrl, user, pw);
-			System.out.println("데이터베이스 연결 성공!!");
-			
-			DeptDao dao = DeptDao.getInstance(); // new DeptDao();
-			
-//--------------------------------------------------------------------------------------------
-			//READ : SELECT
-			
-//			List<Dept> list = dao.getDeptList(conn);
-//			
-//			for(Dept dept : list ) {
-//				System.out.println(dept);
-//			}
-//			*/
-//
-//---------------------------------------------------------------------------------------------			
-			// CREATE: INSERT 
-			
-			Scanner sc = new Scanner(System.in);
-//			System.out.println("부서입력을 시작합니다.");
-//			System.out.println("부서번호  부서이름 부서위치 순으로 입력해주세요.");
-//			System.out.println("예) 50 dev seoul");
-//			String deptData = sc.nextLine();
-//			String[] deptDatas = deptData.split(" "); // 공백 기준으로 잘라서 배열에 넣기
-//			
-//			// Dept 객체 생성하고 Dao 메소드 호출
-//			Dept dept = new Dept(Integer.parseInt(deptDatas[0]), deptDatas[1], deptDatas[2]);
-//			
-//		    int result = dao.insertDept(conn,dept);
-//		    if(result > 0) {
-//		    	System.out.println("입력성공");
-//		    }else {
-//		    	System.out.println("입력 실패!!");
-//		    }
-//-----------------------------------------------------------------------------------------------
-			
-		    // UPDATE : UPDATE
-		    		    
-//			System.out.println("부서수정을 시작합니다.");
-//			System.out.println("수정하고자 하는 부서번호  새로운 부서이름 새로운 부서위치 순으로 입력해주세요.");
-//			System.out.println("예) 50 dev seoul");
-//			String deptData = sc.nextLine();
-//			String[] deptDatas = deptData.split(" "); // 공백 기준으로 잘라서 배열에 넣기
-//			
-//			// Dept 객체 생성하고 Dao 메소드 호출
-//			Dept dept = new Dept(Integer.parseInt(deptDatas[0]), deptDatas[1], deptDatas[2]);
-//			
-//		    int result = dao.editDept(conn,dept);
-//		    if(result > 0) {
-//		    	System.out.println("수정성공");
-//		    }else {
-//		    	System.out.println("수정 실패!!");
-//		    }
-		    
-		    
-//------------------------------------------------------------------------------------------------		
-			// DELETE : DELETE
-			
-			System.out.println("부서정보를 삭제합니다.");
-			System.out.println("삭제할 부서번호를 입력해주세요.");
-			String deptno = sc.nextLine();
-			
-			int result = dao.deleteDept(conn, Integer.parseInt(deptno));
-			
-			if(result > 0) {
-				System.out.println("삭제 완료 ");
-			}else {
-				System.out.println("삭제 실패 : 해당 번호의 부서가 존재하지 않습니다.");
+				switch(num) {
+				case 1:
+					manager.deptList();
+					break;
+				case 2:
+					manager.inputData();
+					break;
+				case 3:
+					manager.editData();
+					break;
+				case 4:
+					manager.deleteData();
+					break;
+				case 5:
+					System.out.println("프로그램을 종료합니다.");
+					System.exit(0);
+					break;
+
+				}
+
 			}
-			
+
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		} 
 
-		
 	}
 
 }
