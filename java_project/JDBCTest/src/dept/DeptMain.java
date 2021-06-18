065ebd8c98471a50d6dc415e86d4c6d6072e47a6
SeraWi <wsr2357@naver.com>
2021-06-18 16:55:45 +0900

    Update DeptMain.java

--- java_project/JDBCTest/src/dept/DeptMain.java
+++ java_project/JDBCTest/src/dept/DeptMain.java
@@ -28,7 +28,8 @@ public class DeptMain {
 			
 			DeptDao dao = new DeptDao();
 			
-//		         리스트 출력 READ
+//--------------------------------------------------------------------------------------------
+			//READ : SELECT
 			
 //			List<Dept> list = dao.getDeptList(conn);
 //			
@@ -60,21 +61,39 @@ public class DeptMain {
 			
 		    // Update : update
 		    		    
-			System.out.println("부서수정을 시작합니다.");
-			System.out.println("수정하고자 하는 부서번호  새로운 부서이름 새로운 부서위치 순으로 입력해주세요.");
-			System.out.println("예) 50 dev seoul");
-			String deptData = sc.nextLine();
-			String[] deptDatas = deptData.split(" "); // 공백 기준으로 잘라서 배열에 넣기
+//			System.out.println("부서수정을 시작합니다.");
+//			System.out.println("수정하고자 하는 부서번호  새로운 부서이름 새로운 부서위치 순으로 입력해주세요.");
+//			System.out.println("예) 50 dev seoul");
+//			String deptData = sc.nextLine();
+//			String[] deptDatas = deptData.split(" "); // 공백 기준으로 잘라서 배열에 넣기
+//			
+//			// Dept 객체 생성하고 Dao 메소드 호출
+//			Dept dept = new Dept(Integer.parseInt(deptDatas[0]), deptDatas[1], deptDatas[2]);
+//			
+//		    int result = dao.editDept(conn,dept);
+//		    if(result > 0) {
+//		    	System.out.println("수정성공");
+//		    }else {
+//		    	System.out.println("수정 실패!!");
+//		    }
+		    
+		    
+//------------------------------------------------------------------------------------------------		
+			// DELETE : delete
 			
-			// Dept 객체 생성하고 Dao 메소드 호출
-			Dept dept = new Dept(Integer.parseInt(deptDatas[0]), deptDatas[1], deptDatas[2]);
+			System.out.println("부서정보를 삭제합니다.");
+			System.out.println("삭제할 부서번호를 입력해주세요.");
+			String deptno = sc.nextLine();
 			
-		    int result = dao.editDept(conn,dept);
-		    if(result > 0) {
-		    	System.out.println("수정성공");
-		    }else {
-		    	System.out.println("수정 실패!!");
-		    }
+			int result = dao.deleteDept(conn, Integer.parseInt(deptno));
+			
+			if(result > 0) {
+				System.out.println("삭제 완료 ");
+			}else {
+				System.out.println("삭제 실패 : 해당 번호의 부서가 존재하지 않습니다.");
+			}
+			
+			
 		    
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -83,13 +102,7 @@ public class DeptMain {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 
-//------------------------------------------------------------------------------------------------		
-		// DELETE : delete
-		
-		
-		
 		
 	}
 
