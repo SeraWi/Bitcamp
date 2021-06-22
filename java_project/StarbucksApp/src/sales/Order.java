80a749c1186fa5122e3ca03cc98d2245678a1629
SeraWi <wsr2357@naver.com>
2021-06-22 15:22:14 +0900

    Update Order.java

--- java_project/StarbucksApp/src/sales/Order.java
+++ java_project/StarbucksApp/src/sales/Order.java
@@ -81,26 +81,35 @@ public class Order {
 				pstmt= conn.prepareStatement(sql);
 				
 				// 0이 입력되었을 때는 저장하지 않는다. 
+				// 반복문 처리한다.
+				if( ameNum != 0) {
 				pstmt.setString(1, "americano");
 				pstmt.setInt(2, americano* ameNum);
 				result = pstmt.executeUpdate();
+				}
 				
+				if(latteNum !=0) {
 				pstmt.setString(1,"latte");
 				pstmt.setInt(2, latte * latteNum);
 				result = pstmt.executeUpdate();
-								
+				}
+				
+				if(sandNum != 0) {
 				pstmt.setString(1, "sandwich");
 				pstmt.setInt(2, sandwich* sandNum);
 				result = pstmt.executeUpdate();
+				}
 				
+				if(saladNum != 0) {
 				pstmt.setString(1, "salad");
 				pstmt.setInt(2, salad* saladNum);
 				result = pstmt.executeUpdate();
-				
+				}
+				if(cakeNum != 0) {
 				pstmt.setString(1, "cake");
 				pstmt.setInt(2, cake* cakeNum);
 				result = pstmt.executeUpdate();
-				
+				}
 				
 				if(result > 0) {
 					System.out.println("입력되었습니다.");
@@ -109,36 +118,37 @@ public class Order {
 				}
 				
 				
+				int totalPrice = americano * ameNum + latte * latteNum + sandwich * sandNum + salad * saladNum + cake * cakeNum;
+				System.out.println(" 총 결제 금액: " +totalPrice);
+				
+				int point = (int)(totalPrice * 0.01);
+				System.out.println("예상 적립 포인트: " + point);
+				
+				
+				System.out.println("------------------------------------");
+				System.out.println("결제합니다.");
+				System.out.println("현재 사용가능한 포인트: ");
+				// 회원 DB에서 point를 read하기
+
+				System.out.println("포인트를 사용하시겠습니까?(포인트를 사용시 현재 결제하시는 상품의 포인트는 적립이 되지 않습니다.)");
+				// 회원이 point 사용할시 회원 DB에서 point update하기 
+				
+				System.out.println("------------------------------------");
+				
+				System.out.println("포인트가 A점 적립되었습니다.");
+				//회원이 포인트 사용하지 않고 결제 할 경우
+				//회원 DB에서 point update하기
+				//결제 한후, 회원이 산거 판매DB에 저장하기
+				
+				
+				
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-			int totalPrice = americano * ameNum + latte * latteNum + sandwich * sandNum + salad * saladNum + cake * cakeNum;
-			System.out.println(" 총 결제 금액: " +totalPrice);
-			
-			int point = (int)(totalPrice * 0.01);
-			System.out.println("예상 적립 포인트: " + point);
-			
-			
-			System.out.println("------------------------------------");
-			System.out.println("결제합니다.");
-			System.out.println("현재 사용가능한 포인트: ");
-			// 회원 DB에서 point를 read하기
-
-			System.out.println("포인트를 사용하시겠습니까?(포인트를 사용시 현재 결제하시는 상품의 포인트는 적립이 되지 않습니다.)");
-			// 회원이 point 사용할시 회원 DB에서 point update하기 
-			
-			System.out.println("------------------------------------");
-			
-			System.out.println("포인트가 A점 적립되었습니다.");
-			//회원이 포인트 사용하지 않고 결제 할 경우
-			//회원 DB에서 point update하기
-			//결제 한후, 회원이 산거 판매DB에 저장하기
-
-			
+		
 	
 		}
 }
