817c63d485ceceab1fabe43a76593034148bab45
SeraWi <wsr2357@naver.com>
2021-06-22 16:25:22 +0900

    Update Order.java

--- java_project/StarbucksApp/src/sales/Order.java
+++ java_project/StarbucksApp/src/sales/Order.java
@@ -22,6 +22,7 @@ public class Order {
 			
 			
 			System.out.println("주문하기");
+			
 			System.out.println("메뉴 입니다.");
 			System.out.println("------------------------------------");
 			System.out.println("1. Amerciano : 4100");
@@ -29,36 +30,37 @@ public class Order {
 			System.out.println("3. Sandwich : 6200");
 			System.out.println("4. salad : 5000");
 			System.out.println("5. cake : 5500");
+			System.out.println("6. 주문 완료");
 			System.out.println("------------------------------------");
 			
-			System.out.println("메뉴의 수량을 입력해주세요.");
-			System.out.print("1. Amerciano > ");
-			int ameNum = Integer.parseInt(scanner.nextLine());
-			System.out.print("2. Latter > ");
-			int latteNum = Integer.parseInt(scanner.nextLine());
-			System.out.print("3. Sandwich > ");
-			int sandNum = Integer.parseInt(scanner.nextLine());
-			System.out.print("4. salad > ");
-			int saladNum = Integer.parseInt(scanner.nextLine());
-			System.out.print("5. cake > " );
-			int cakeNum = Integer.parseInt(scanner.nextLine());
+			System.out.println("원하시는 메뉴의 번호와 수량을 입력하세요.");
+			System.out.println("예시)1 3");
+			String inputData = scanner.nextLine();
+			String[] inputDatas = inputData.split(" ");
 			
-			System.out.println("------------------------------------");
-			System.out.println(" 주문 내역 및  포인트 적립");
+			//샀을 때 sale객체로 정보 넣기
+			ArrayList<Sale> list = new ArrayList<>();
 			
-							
+			switch(inputDatas[0]) {
 			
-//			//샀을 때 sale객체로 정보 넣기
-//			ArrayList<Sale> list = new ArrayList<>();
-//			
-//			// 0개일때는 sale객체 넣을 필요 없다.
-//			list.add(new Sale("americano", ameNum));
-//			list.add(new Sale("latte", latteNum));
-//			list.add(new Sale("sandwich", sandNum));
-//			list.add(new Sale("salad", saladNum));
-//			list.add(new Sale("cake", cakeNum ));
-//			
-			//DB에 insert하기
+			case "1":
+				list.add(new Sale("americano", Integer.parseInt(inputDatas[1])*americano));
+				break;
+			case "2":
+				list.add(new Sale("latte", Integer.parseInt(inputDatas[1])*latte));
+				break;
+			case "3":
+				list.add(new Sale("sandwich", Integer.parseInt(inputDatas[1])*sandwich));
+				break;
+			case "4":
+				list.add(new Sale("salad", Integer.parseInt(inputDatas[1])*salad));
+				break;
+			case "5": 
+				list.add(new Sale("cake", Integer.parseInt(inputDatas[1])*cake));
+				break;
+			case "6":
+				System.exit(0);
+			}
 			
 			
 			Connection conn = null;
@@ -77,58 +79,55 @@ public class Order {
 
 				conn= DriverManager.getConnection(jdbcUrl, user, pw);
 				
-				String sql = "insert into sale (salecode, sname, price) values(sale_salecode_seq.nextval, ?, ?)";
-				pstmt= conn.prepareStatement(sql);
-				
-				// 0이 입력되었을 때는 저장하지 않는다. 
-				// 반복문 처리한다.
-				if( ameNum != 0) {
-				pstmt.setString(1, "americano");
-				pstmt.setInt(2, americano* ameNum);
-				result = pstmt.executeUpdate();
+				// sql 작성 ->list 반복하기
+				for(int i = 0; i <list.size(); i++) {
+					String sql = "insert into sale (salecode, sname, price) values(sale_salecode_seq.nextval, ?, ?)";
+					pstmt= conn.prepareStatement(sql);
+					pstmt.setString(1,list.get(i).getSname());
+					pstmt.setInt(2, list.get(i).getPrice());
+					result = pstmt.executeUpdate();
 				}
-				
-				if(latteNum !=0) {
-				pstmt.setString(1,"latte");
-				pstmt.setInt(2, latte * latteNum);
-				result = pstmt.executeUpdate();
-				}
-				
-				if(sandNum != 0) {
-				pstmt.setString(1, "sandwich");
-				pstmt.setInt(2, sandwich* sandNum);
-				result = pstmt.executeUpdate();
-				}
-				
-				if(saladNum != 0) {
-				pstmt.setString(1, "salad");
-				pstmt.setInt(2, salad* saladNum);
-				result = pstmt.executeUpdate();
-				}
-				if(cakeNum != 0) {
-				pstmt.setString(1, "cake");
-				pstmt.setInt(2, cake* cakeNum);
-				result = pstmt.executeUpdate();
-				}
+
 				
 				if(result > 0) {
 					System.out.println("입력되었습니다.");
 				}else {
 					System.out.println("입력실패!");
 				}
+			
+//--------------------------------------------------------------------------------------------------------------------------------------				
+				System.out.println("----------------------------------------");
+				int totalPrice = 0;
 				
+				for(int i = 0; i <list.size(); i++) {
+					totalPrice += list.get(i).getPrice();
+				}
 				
-				int totalPrice = americano * ameNum + latte * latteNum + sandwich * sandNum + salad * saladNum + cake * cakeNum;
-				System.out.println(" 총 결제 금액: " +totalPrice);
 				
 				int point = (int)(totalPrice * 0.01);
-				System.out.println("예상 적립 포인트: " + point);
+				
+				System.out.println("총 예상 결제 금액: " +totalPrice +"원 입니다.");	
+				System.out.println("총 예상 적립 포인트:"+ point +"점입니다.");
+				
+//--------------------------------------------------------------------------------------------------------------------------------------				
+				
+				System.out.println("---------------------------------------");
 				
 				
-				System.out.println("------------------------------------");
+				// 회원 DB에서 point를 read하기
+				
+				String readPoint = "select point from member where id = ?";
+				pstmt= conn.prepareStatement(readPoint);
+				
+				pstmt.setString(1, "lee1234");
+				result = pstmt.executeUpdate();
+				
 				System.out.println("결제합니다.");
 				System.out.println("현재 사용가능한 포인트: ");
-				// 회원 DB에서 point를 read하기
+				
+				
+				
+				
 
 				System.out.println("포인트를 사용하시겠습니까?(포인트를 사용시 현재 결제하시는 상품의 포인트는 적립이 되지 않습니다.)");
 				// 회원이 point 사용할시 회원 DB에서 point update하기 
@@ -140,6 +139,10 @@ public class Order {
 				//회원 DB에서 point update하기
 				//결제 한후, 회원이 산거 판매DB에 저장하기
 				
+				//포인트 적립 ->회원 테이블에 DB 저장하기
+				
+				
+				
 				
 				
 			} catch (ClassNotFoundException e) {
@@ -148,7 +151,8 @@ public class Order {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+
+			
 		
-	
-		}
+			}
 }
