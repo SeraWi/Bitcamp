6d7320fc3a2c357969774dc8e87c8960ead1d7f5
SeraWi <wsr2357@naver.com>
2021-06-22 18:34:11 +0900

    Update SaleMain.java

--- java_project/StarbucksApp/src/sales/SaleMain.java
+++ java_project/StarbucksApp/src/sales/SaleMain.java
@@ -8,10 +8,6 @@ public class SaleMain {
 		SaleManager manager = new SaleManager(SaleDao.getInstance());
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("1. 주문하기  2. 내 포인트 확인하기 3. 내 정보 보기");
-		System.out.println(" 주문하기 입니다. ");
-		//order 인스턴스 생성해서 쓰기
-		
 		
 		try {
 			//1. 드라이버 로드
@@ -20,8 +16,7 @@ public class SaleMain {
 			
 			System.out.println("관리자가 판매 테이블을 조회합니다. ");
 			manager.saleList();
-			
-			
+				
 			
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
