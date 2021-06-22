663946f33da37d8caee9b5183a3a5fc48ed5ebfe
SeraWi <wsr2357@naver.com>
2021-06-22 17:32:10 +0900

    Update Order.java

--- java_project/StarbucksApp/src/sales/Order.java
+++ java_project/StarbucksApp/src/sales/Order.java
@@ -140,7 +140,7 @@ public class Order {
 				currPoint = rs.getInt("point");
 			}
 			
-			System.out.println("결제합니다.");
+			
 			System.out.println("현재 사용가능한 포인트: " + currPoint);
 			System.out.println("포인트를 사용하시겠습니까? 1. 예 2. 아니오");
 			System.out.println("(포인트를 사용할시 현재 결제하시는 상품의 포인트는 적립이 되지 않습니다.)");
@@ -149,11 +149,22 @@ public class Order {
 			System.out.println("------------------------------------");
 			
 			//DB에 포인트 적립하기 ->update 하기
+			String updatePoint = "update member set point= point + ? where id = ? ";
+			pstmt = conn.prepareStatement(updatePoint);
 			
+			pstmt.setInt(1, point);
+			pstmt.setString(2, memId);
 			
+			result = pstmt.executeUpdate();
+			
+			if(result > 0) {
+				System.out.println("업데이트 완료");
+			}else {
+				System.out.println("업데이트 실패");
+			}
 			
 
-			System.out.println("포인트가 A점 적립되었습니다.");
+			System.out.println("포인트가 "+point+"점 적립되어"+ currPoint+" 점 있습니다.");
 			//회원이 포인트 사용하지 않고 결제 할 경우
 			//회원 DB에서 point update하기
 			//결제 한후, 회원이 산거 판매DB에 저장하기
