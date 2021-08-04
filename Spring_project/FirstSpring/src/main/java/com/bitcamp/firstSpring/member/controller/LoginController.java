140fff41e0a2ea757a0265fbd48f6084d3ff5484
SeraWi <wsr2357@naver.com>
2021-08-04 12:45:12 +0900

    Spring-Project: FirstSpring : LoginController, Login./form

--- Spring_project/FirstSpring/src/main/java/com/bitcamp/firstSpring/member/controller/LoginController.java
+++ Spring_project/FirstSpring/src/main/java/com/bitcamp/firstSpring/member/controller/LoginController.java
@@ -1,15 +1,64 @@
 package com.bitcamp.firstSpring.member.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bitcamp.firstSpring.member.domain.LoginRequest;
 
 @Controller
+@RequestMapping("/member/login") //브라우저에서 사용하는 URL http://localhost:8080/firstSpring/member/login
 public class LoginController {
 
-	@RequestMapping("/member/loginForm")
-	public String getLoginForm() {
-		return "member/loginForm";  //   /WEB-INF/views/member/loginForm.jsp
+	//@RequestMapping("/member/loginForm")
+	//@RequestMapping(value="/member/login", method =RequestMethod.GET)
+	@RequestMapping(method=RequestMethod.GET)
+	public String getLoginForm(
+			@RequestParam(value ="page" , defaultValue="1") int page
+			) {
+		//System.out.println(page+1);
+		return "member/loginForm";  //   /WEB-INF/views/member/loginForm.jsp -->뷰이름
 	}
 	
+	
+	
+	//사용자의 파라미터 데이터를 받는 방법
+	// 1. HttpServlterRequest 이용하기
+	// 2. @RequestParam("파라미터 이름 param name")
+	// 3. 커맨드 객체(Beans) 이용
+	//@RequestMapping(value="/member/login",method=RequestMethod.POST)
+	@RequestMapping(method=RequestMethod.POST)
+	public String login(
+			@RequestParam("id") String id,
+			@RequestParam("pw")String pw,
+			HttpServletRequest request,
+			@ModelAttribute("loginReg") LoginRequest loginRequest,
+			Model model
+			) {
+		System.out.println(id +":" + pw);
+		
+		//view에 전달할 데이터 저장: Model 객체를 이용 (반환을 String = view만 할 때), 같이 공유한 데이터 전달하려면 Model쓰자)
+		model.addAttribute("id",id);
+		model.addAttribute("pw",pw);
+		
+		///---------------------------------------
+		String uid = request.getParameter("id");
+		String upw = request.getParameter("pw");
+		System.out.println(uid +":"+ upw); //@requestParma 사용
+		
+		model.addAttribute("uid",uid);
+		model.addAttribute("upw",upw);
+		
+		///---------------------------------------
+		System.out.println(loginRequest); //bean 사용
+		//model에 자동 저장된다.
+		
+		return "member/login"; // /WEB-INF/views/member/login.jsp
+	}
 	
 }
--- Spring_project/FirstSpring/src/main/webapp/WEB-INF/views/member/login.jsp
+++ Spring_project/FirstSpring/src/main/webapp/WEB-INF/views/member/login.jsp
@@ -12,31 +12,27 @@
 </script>
 </head>
 <body>
-	<h1>LoginForm</h1>
+	<h1>Login Request Data</h1>
 	<hr>
 	
 	<!-- action 에 대한 속성이 없으면 submit하면 다시 자기 자신으로 돌아온다. 
 		actoin="contextpath/member/login"
 		post로 보내기!
+		
+		보낸 데이터 받아오기
 	-->
-	<form method="post">  
 		<table>
 			<tr>
 				<td>아이디</td>
-				<td><input type="text" name="id" ></td>
+				<td>${id}, ${uid}, ${loginRequest.id}</td>
 			</tr>
 			<tr>
 				<td>비밀번호</td>
-				<td><input type="password" name="pw" ></td>
-			</tr>
-			<tr>
-				<td></td>
-				<td><input type="submit"></td>
+				<td>${pw}, ${upw}, ${loginReg.pw}</td>
 			</tr>
 		
 		</table>
 	
 	
-	</form>
 </body>
 </html>
